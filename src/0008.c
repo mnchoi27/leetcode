88e72d3005f9c9fa1b636fd7b7ad641a80561c77
mnchoi27 <mnchoi27@gmail.com>
2026-08-08 20:10:00 +0900

    feat: add 0008.c

---
+++ src/0008.c
@@ -0,0 +1,30 @@
+int myAtoi(char* s) {
+    int i = 0;
+    int sign = 1;
+    long long ans = 0;
+
+    while (s[i] == ' ') {
+        i++;
+    }
+
+    if (s[i] == '+' || s[i] == '-') {
+        if (s[i] == '-') {
+            sign = -1;
+        }
+        i++;
+    }
+
+    while (isdigit((unsigned char)s[i])) {
+        ans = ans * 10 + (s[i] - '0');
+
+        if (sign * ans > INT_MAX) {
+            return INT_MAX;
+        }
+        if (sign * ans < INT_MIN) {
+            return INT_MIN;
+        }
+        i++;
+    }
+
+    return (int)(sign * ans);
+}
