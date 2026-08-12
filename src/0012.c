70af9eb7c459bfa4abe6c33fac8b44bfc3eb5497
mnchoi27 <mnchoi27@gmail.com>
2026-08-12 11:55:53 +0900

    feat: add 0012.c

---
+++ src/0012.c
@@ -0,0 +1,18 @@
+char* intToRoman(int num) {
+    int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    char* romans[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+    char* ans = malloc(16 * sizeof(char));
+    int pos = 0;
+
+    for (int i = 0; i < 13 && num > 0; i++) {
+        while (num >= values[i]) {
+            num -= values[i];
+            strcpy(ans + pos, romans[i]);
+            pos += strlen(romans[i]);
+        }
+    }
+    ans[pos] = '\0';
+
+    return ans;
+}
