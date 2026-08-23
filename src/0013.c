b3a1e777c3fb4adf9d919403e6dd2d530519fb00
mnchoi27 <mnchoi27@gmail.com>
2026-08-23 13:31:52 +0900

    style: indent switch case bodies in 0013.c

--- src/0013.c
+++ src/0013.c
@@ -1,13 +1,21 @@
 int romanValue(char c) {
     switch (c) {
-        case 'M': return 1000;
-        case 'D': return 500;
-        case 'C': return 100;
-        case 'L': return 50;
-        case 'X': return 10;
-        case 'V': return 5;
-        case 'I': return 1;
-        default: return 0;
+        case 'M':
+            return 1000;
+        case 'D':
+            return 500;
+        case 'C':
+            return 100;
+        case 'L':
+            return 50;
+        case 'X':
+            return 10;
+        case 'V':
+            return 5;
+        case 'I':
+            return 1;
+        default:
+            return 0;
     }
 }
 
